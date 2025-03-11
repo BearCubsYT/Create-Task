78a3b467eb1146ac556182ba96c63e9d6f82ccb6
Jay Payson <jaypayson@webdev09.local>
2025-03-11 13:41:40 -0400

    Started

---
+++ app.js
@@ -0,0 +1,14 @@
+let playerScores = []
+
+let clicker = document.getElementById('clicker')
+let clickerState = 'red'
+let playerLog = document.getElementById('playerLog')
+
+
+clicker.addEventListener('click', function() {
+    if (clickerState == 'red'){
+        clicker.textContent = 'Too Soon!'
+        r
+    }
+    console.log(clicker.style.border)
+});
